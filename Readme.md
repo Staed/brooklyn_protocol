7aa7b76ff692b97314b43e376ad8143bc9c0a8d3
Kelsey <ky.staedler@gmail.com>
2023-04-18 00:38:08 -0400

    Implement multiple peers per node

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Brooklyn Protocol
+
+## What is this?
+
+A blockchain-powered communication P2P protocol emphasizing low latency and low computing overhead.
+
+## Does it work yet?
+
+No, it is still being written.
--- src/main.rs
+++ src/main.rs
@@ -21,21 +21,33 @@ impl Blockchain {
 }
 
 struct Node {
-    peer_addrs: SocketAddr,
+    peer_addrs: Vec<SocketAddr>,
     socket: UdpSocket,
     blockchain: Blockchain,
 }
 
 impl Node {
-    pub async fn new(peer_addrs: SocketAddr, self_addr: SocketAddr, blockchain: Blockchain) -> io::Result<Node> {
+    pub async fn new(peer_addrs: Vec<SocketAddr>, self_addr: SocketAddr, blockchain: Blockchain) -> io::Result<Node> {
         let socket = UdpSocket::bind(self_addr).await?;
         Ok(Node { peer_addrs, socket, blockchain })
     }
 
-    pub async fn send_message(&self, message: &[u8]) -> Result<usize, Box<dyn std::error::Error>> {
-        let size = self.socket.send_to(message, self.peer_addrs).await?;
-        eprintln!("Sent {} bytes to {}", message.len(), self.peer_addrs);
-        Ok(size)
+    pub fn add_peer(mut self, new_addr: SocketAddr) {
+        // TODO Check for isize::MAX panic
+        self.peer_addrs.push(new_addr.clone())
+    }
+
+    // TODO Ping every X time and remove peer from peer_addrs if no response
+
+    pub async fn broadcast_message(&self, message: &[u8]) -> Result<usize, Box<dyn std::error::Error>> {
+        // TODO Parallelize this
+        let mut total_sent = 0;
+        for addr in self.peer_addrs.iter() {
+            let size = self.socket.send_to(message, addr).await?;
+            eprintln!("Sent {} bytes to {}", message.len(), addr);
+            total_sent += size;
+        }
+        Ok(total_sent)
     }
 
     pub async fn poll_messages(&self, buf: &mut [u8]) -> Result<(usize, SocketAddr), Box <dyn std::error::Error>> {
@@ -55,22 +67,42 @@ impl Node {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let node1_addr = "127.0.0.1:9000".parse().unwrap();
-    let node2_addr = "127.0.0.1:9001".parse().unwrap();
-    let blockchain = Blockchain::new(835);
+    let node1_addr: SocketAddr = "127.0.0.1:9000".parse().unwrap();
+    let node2_addr: SocketAddr = "127.0.0.1:9001".parse().unwrap();
+    let node3_addr: SocketAddr = "127.0.0.1:9002".parse().unwrap();
+    let blockchain: Blockchain = Blockchain::new(835);
 
+    let mut node1_peers: Vec<SocketAddr> = Vec::new();
+    let mut node2_peers: Vec<SocketAddr> = Vec::new();
+    let mut node3_peers: Vec<SocketAddr> = Vec::new();
 
-    let node1 = Node::new(node2_addr, node1_addr, blockchain.clone()).await?;
-    let node2 = Node::new(node1_addr, node2_addr, blockchain.clone()).await?;
+    // TODO Find a way to not have to clone these addresses constantly
+    node1_peers.push(node2_addr.clone());
+    node1_peers.push(node3_addr.clone());
+
+    node2_peers.push(node1_addr.clone());
+    node2_peers.push(node3_addr.clone());
+
+    node3_peers.push(node1_addr.clone());
+    node3_peers.push(node2_addr.clone());
+
+    let node1 = Node::new(node1_peers, node1_addr, blockchain.clone()).await?;
+    let node2 = Node::new(node2_peers, node2_addr, blockchain.clone()).await?;
+    let node3 = Node::new(node3_peers, node3_addr, blockchain.clone()).await?;
 
     thread::sleep(Duration::from_secs(1));
     let message = "Hello world".as_bytes();
-    node1.send_message(message).await?;
+    node1.broadcast_message(message).await?;
 
-    let mut buffer = [0u8; 1024];
-    let (size, addr) = node2.poll_messages(&mut buffer).await?;
+    let mut buffer_2 = [0u8; 1024];
+    let node2_future = node2.poll_messages(&mut buffer_2);
+    let mut buffer_3 = [0u8; 1024];
+    let node3_future = node3.poll_messages(&mut buffer_3);
 
-    println!("Node2 received {} bytes from {}: {:?}", size, addr, &buffer[..size]);
+    let (size_2, addr_2) = node2_future.await?;
+    let (size_3, addr_3) = node3_future.await?;
+    println!("Node2 received {} bytes from {}: {:?}", size_2, addr_2, &buffer_2[..size_2]);
+    println!("Node3 received {} bytes from {}: {:?}", size_3, addr_3, &buffer_3[..size_3]);
 
     Ok(())
 }
